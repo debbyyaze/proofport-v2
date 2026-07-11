e81eea5239e63339a9fcd69a08e212756d7e217e
debbyyaze <debbyyazevedo@gmail.com>
2026-07-11 07:03:30 -0300

    Stabilize web app manifest id

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,6 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: "/",
     name: "ProofPort",
     short_name: "ProofPort",
     description:
