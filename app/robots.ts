cd35105e365d12fccf34f5948760a45728e795d0
debbyyaze <debbyyazevedo@gmail.com>
2026-07-20 15:04:22 -0300

    Exclude API routes from robots

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/"
+      allow: "/",
+      disallow: "/api/"
     },
     host: appUrl,
     sitemap: `${appUrl}/sitemap.xml`
